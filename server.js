b816dc4fdf91d4fd588b417ee9cb5046174c698b
HaroldF415 <harold.f415@gmail.com>
2023-04-21 06:04:20 -0400

    backend complete

---
+++ server.js
@@ -0,0 +1,10 @@
+// DEPENDENCIES
+require("dotenv").config();
+const app = require("./app.js");
+
+const PORT = process.env.PORT;
+
+// LISTEN
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
